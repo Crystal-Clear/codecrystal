3bab30760ee2eaeb117add13445375fdb57191dd
Nikhila Ravi <nikkiravi92@gmail.com>
2015-06-26 14:45:07 +0100

    added all organisation repos

--- makeRepoList.js
+++ makeRepoList.js
@@ -1,18 +1,29 @@
-module.exports = function (repoArray, orgRepoArry) {
+module.exports = function (repoArray, orgRepoArray) {
 
-  console.log('repoArray', repoArray);
+  // console.log('orgRepoArray', orgRepoArray);
   // var repos = repoArray.map(function(elem) { return elem[0]; });
 
-  var makeRepoListString = repoArray.map(function(repo) { return makeRepoButton(repo);});
+  var makeUserRepoListString = repoArray.map(function(repo) { return makeRepoButton(repo);});
+
+  var makeOrgRepoListString = orgRepoArray.map(function(org) { return makeOrgButtons(org); });
 
   function makeRepoButton(repo) {
     return '<button class="repoButton"><a href=/graph/' + repo[0] + "/" + repo[1] + '>' + repo[0].split('/').pop() + '</a></button>';
+  }
+
+  function makeOrgButtons(org) {
+    var orgButtons = '<div class="org"><div class="orgName">' + org.org + '</div>';
+    // console.log(org);
+    org.repos.forEach(function(repo) {
+      orgButtons += '<button class="repoButton"><a href=/graph/' + org.org + "/" + repo[0] + "/" + repo[1] + '>' + repo[0] + '</a></button>';
+    });
+    return orgButtons + '</div>';
   }
 
   var htmlTop = '<!DOCTYPE html><html><head><meta charset="UTF-8"><link rel="stylesheet" href="/static/css/main.css"><link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,600" rel="stylesheet" type="text/css"><meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0"/></head><body><div id="content"><h1>Choose your GitHub repos<h1><div id="repoList">';
 
   var htmlBottom = '</div></div><script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script></body></html>';
 
-  return (htmlTop + makeRepoListString.join('') + htmlBottom);
+  return (htmlTop + makeUserRepoListString.join('') + makeOrgRepoListString.join('') + htmlBottom);
 
 };
