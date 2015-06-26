44fafcae428ad74569d120cca0040adb0bdef2a0
rjmk <rafekennedy@gmail.com>
2015-06-26 16:18:20 +0100

    header fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 document.getElementById('header').addEventListener('click',function(){
   var visibilities = ['block', 'none'];
   var currentVis = document.getElementById('legend').style.display;
-  console.log(currentVis);
   document.getElementById('legend').style.display = visibilities[visibilities.indexOf(currentVis) + 1] || 'block';
 });
