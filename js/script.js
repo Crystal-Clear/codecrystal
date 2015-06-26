00a29b20569dd1fbe237c6c6f056c3c87b1d1ddc
Nikhila Ravi <nikkiravi92@gmail.com>
2015-06-26 16:38:58 +0100

    badge button

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ document.getElementById('header').addEventListener('click',function(){
   console.log(currentVis);
   document.getElementById('legend').style.display = visibilities[visibilities.indexOf(currentVis) + 1] || 'block';
 });
+
+
+$('body').on('mouseover','.repoLink', function() {
+  console.log(this.href);
+  document.getElementById('badgeLink').innerHTML = '[![Codecrystal](https://img.shields.io/badge/code-crystal-5CB3FF.svg)](' + this.href + ')';
+});
